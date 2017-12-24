d14de5f052a249fb4d318bfeb86f7c8e9b974a84
nieroda <eathotlead@gmail.com>
2017-12-24 17:09:28 -0600

    fixed bug

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var IDChecked = [];
 const safeCheck = true; //HIGHLY recomended~
 
 /* See Documentation on poloniex.js */
-const polo = new Poloniex(API_KEY, SECRET, 0, 0);
+const polo = new Poloniex(API_KEY, SECRET, 0.0001, 0);
 
 const T = new Twit({
   consumer_key: '',
@@ -18,11 +18,18 @@ const T = new Twit({
   access_token_secret: ''
 });
 
+//961445378
 var stream = T.stream('statuses/filter', { follow : '961445378' });
 
 stream.on('tweet', (tweet, err) => {
     if (!isIn(tweet.id)) {
-      checkTweet(tweet.text);
+      if (tweet.user.id_str === '961445378') {
+        if (validate(tweet)) {
+          checkTweet(tweet.text);
+        } else {
+          console.log('Bad Tweet, IGNORING')
+        }
+      }
     } else {
       console.log(`Found tweet ${tweet.id} but it has already been processed`);
     }
@@ -57,9 +64,17 @@ function isIn(id) {
   return false;
 }
 
+function validate(twitter_response) {
+ let { in_reply_to_status_id, in_reply_to_status_id_str, in_reply_to_user_id, in_reply_to_user_id_str, in_reply_to_screen_name } = twitter_response;
+
+ if (!in_reply_to_status_id && !in_reply_to_status_id_str && !in_reply_to_user_id && !in_reply_to_user_id_str && !in_reply_to_screen_name) {
+   return true;
+ } else {
+   return false;
+ }
+}
 
 /*
-Make sure below is commented out otherwise it will go bad
 Test @ works Made Purchase {"orderNumber":"91514514058","resultingTrades":[{"amount":"0.00270000","date":"2017-12-24 ","rate":"0.03711015","total":"0.00010019","tradeID":"","type":"buy"}]}
 //let a = 'zec coin of the day';
 
