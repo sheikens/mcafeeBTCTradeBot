4aa0cc463fd4f6c7a48aabbdeda2f71d313f9a37
sheikens <Sander@Sanders-MacBook-Air.local>
2017-12-28 15:14:52 +0700

    added poloniex & twitter keys

--- index.js
+++ index.js
@@ -2,34 +2,28 @@ const Twit = require('twit');
 const names = require('./names.js');
 const Poloniex = require('./poloniex');
 
-const API_KEY = '';
-const SECRET = '';
+const API_KEY = '129OP4XW-XDO7RGHS-Q4O7MQTF-60VUDCM4';
+const SECRET = '694683a16bcd5a56cc88dde9ee81d25de52acba612a2a09836e5a508d659b4e2239e4459735f506396f3a52408aa504a01571c7110b16e4e53521803dbcc807c';
 
-var IDChecked = [];     /* Dont Change this value */
-var CoinsBought = [];   /* Dont Change this value */
+var IDChecked = [];
 const safeCheck = true; //HIGHLY recomended~
 
-const buyAmount = 0.0008;   /* Change this value to the amount you want to purchase for example 0.01 will purchase 0.01BTC worth of coin */
-const markup = 0;           /* % over market you want to send an order for incase other bot beats you */
-const resell = false;        /* set true if you want the bot to immediately resell the shitcoin, false if you want to hodl said shitcoin */
-const sellMarkup = 1;    /* markup you want the bot to sell it at for example 1.15 will resell at purchased price + 15% */
 /* See Documentation on poloniex.js */
-/* constructor(API_KEY, SECRET, buyAmount, markup, resell, sellmarkup = 1) */
-const polo = new Poloniex(API_KEY, SECRET, buyAmount, markup, resell, sellMarkup);
+const polo = new Poloniex(API_KEY, SECRET, 0.0001, 0);
 
 const T = new Twit({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token: '',
-  access_token_secret: ''
+  consumer_key: 'oyDuDzWAfp1OJc6MNPxVtbXVP',
+  consumer_secret: 'BjaesHNml3tbg8N8Mv8UfpdxPyR2YZWgnqU6FfYJa4i3bT0NoE',
+  access_token: '26206146-2vpWFK2G9HC6p4OftJBX8wCWFVjtLxF4jnYto1xge',
+  access_token_secret: 'mHY2bHRjj1iKSNVXEwCCwUCZfuttZiehOZnSZf8B3Ug00'
 });
 
+console.log('start')
 //961445378
-
 var stream = T.stream('statuses/filter', { follow : '961445378' });
 
 stream.on('tweet', (tweet, err) => {
-    if (!isIn(tweet.id, IDChecked)) {
+    if (!isIn(tweet.id)) {
       if (tweet.user.id_str === '961445378') {
         if (validate(tweet)) {
           checkTweet(tweet.text);
@@ -42,39 +36,33 @@ stream.on('tweet', (tweet, err) => {
     }
 })
 
-
 function checkTweet(text){
   text = text.toLowerCase();
+  console.log('checkTweet')
   if (safeCheck) {
     for (var val of names) {
-      val = val.toLowerCase();
-      if (text.includes(val) && text.includes('coin of the day')) {
+      if (text.includes(val.toLowerCase()) && text.toLowerCase().includes('coin of the day')) {
         console.log(`${text} :: ${val}`);
-        if (!isIn(val, CoinsBought)) {
-          polo.checkBalancesandBuy(val.toUpperCase());
-        }
+        polo.checkBalancesandBuy(val.toUpperCase());
       }
     }
   } else {
     for (var val of names) {
-      val = val.toLowerCase();
       if (text.includes(val)) {
         console.log(`${text} :: ${val}`);
-        if (!isIn(val, CoinsBought)) {
-          polo.checkBalancesandBuy(val.toUpperCase());
-        }
+        polo.checkBalancesandBuy(val.toUpperCase());
       }
     }
   }
 }
 
-function isIn(id, array) {
-  for (var val of array) {
+function isIn(id) {
+  for (var val of IDChecked) {
     if (val === id) {
       return true;
     }
   }
-  array.push(id);
+  IDChecked.push(id);
   return false;
 }
 
@@ -84,17 +72,13 @@ function validate(twitter_response) {
  if (!in_reply_to_status_id && !in_reply_to_status_id_str && !in_reply_to_user_id && !in_reply_to_user_id_str && !in_reply_to_screen_name) {
    return true;
  } else {
-   console.log('Tweet is not valid');
    return false;
  }
 }
 
 /*
-test
-let string = 'coin of the day xrp';
-checkTweet(string);
-coin of the day xrp :: xrp
-Made Purchase {"orderNumber":"103638560814","resultingTrades":[{"amount":"11.05890000","date":"2017-12-25 22:24:37","rate":"0.00007234","total":"0.00080000","tradeID":"16945027","type":"buy"}]}
-Selling now :: Sell price: 0.00007234 :: currencyPair: BTC_XRP :: Amount : 011.05890000
-{ orderNumber: '103638566808', resultingTrades: [] }
+Test @ works Made Purchase {"orderNumber":"91514514058","resultingTrades":[{"amount":"0.00270000","date":"2017-12-24 ","rate":"0.03711015","total":"0.00010019","tradeID":"","type":"buy"}]}
+//let a = 'zec coin of the day';
+
+//checkTweet(a);
 */
