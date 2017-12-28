e62ccf5206f0cacd3eef2ed47e3216c5a9798699
sheikens <Sander@Sanders-MacBook-Air.local>
2017-12-28 16:18:24 +0700

    other try

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+};
+
 const Twit = require('twit');
 const names = require('./names.js');
 const Poloniex = require('./poloniex');
-const PORT = process.env.PORT || 5000
 
 const API_KEY = '129OP4XW-XDO7RGHS-Q4O7MQTF-60VUDCM4';
 const SECRET = '694683a16bcd5a56cc88dde9ee81d25de52acba612a2a09836e5a508d659b4e2239e4459735f506396f3a52408aa504a01571c7110b16e4e53521803dbcc807c';
