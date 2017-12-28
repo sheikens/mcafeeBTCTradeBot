6113dbc97c25834f256df91878e1aa1aaca380bd
sheikens <Sander@Sanders-MacBook-Air.local>
2017-12-28 16:55:37 +0700

    try to get it running

--- index.js
+++ index.js
@@ -20,6 +20,18 @@ const T = new Twit({
 
 console.log('start')
 //961445378
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+var router = require("./router");
+app.use('/viatges', router);
+
+app.listen(process.env.PORT || 3000 ,function(){
+    console.log("up and running on port "+process.env.PORT);
+});
+
+
 var stream = T.stream('statuses/filter', { follow : '961445378' });
 
 stream.on('tweet', (tweet, err) => {
